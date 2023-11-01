548563cf449e8a1156af3078aa685c38ea8339d3
parthvaishnav <parthvaishnavj@gmail.com>
2023-11-01 14:35:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_10_21012011155/Contact.kt
@@ -0,0 +1,6 @@
+package com.example.mad_practical_10_21012011155
+
+import java.io.Serializable
+
+class Contact(var name: String , var dob:String, var address: String ):Serializable {
+}
---
+++ app/src/main/java/com/example/mad_practical_10_21012011155/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mad_practical_10_21012011155
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mad_practical_10_21012011156.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
